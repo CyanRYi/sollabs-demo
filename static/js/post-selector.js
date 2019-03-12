014598a635c257c4aff161e69ed9eed83a943b2b
Cyan Raphael Yi <cyan.r.yi@gmail.com>
2019-03-13 06:51:59 +0900

    logger 추가

--- static/js/post-selector.js
+++ static/js/post-selector.js
@@ -2,8 +2,12 @@
     var checkboxes = document.getElementsByClassName('mdl-checkbox__input');
 
     for(var i = 0; i < checkboxes.length; i++) {
+
+        console.log("target checkboxes : " + checkboxes.length);
         checkboxes[i].addEventListener('change', function() {
-            var posts = document.getElementsByClassName(checkboxes.id);
+
+            console.log(checkboxes[i] + 'changed ' + checkboxes[i].checked);
+            var posts = document.getElementsByClassName(checkboxes[i].id);
 
             for(var j = 0; j < posts.length; j++) {
                 if (checkboxes[i].checked) {
