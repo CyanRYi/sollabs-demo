a51e6dac1213d3d6961dbf9a8e7d6bdc2203b693
Cyan Raphael Yi <cyan.r.yi@gmail.com>
2019-03-13 07:02:19 +0900

    로그 제거하고 display로 마저 수정

--- static/js/post-selector.js
+++ static/js/post-selector.js
@@ -3,17 +3,15 @@
 
     for(var i = 0; i < checkboxes.length; i++) {
 
-        console.log("target checkboxes : " + checkboxes.length);
         checkboxes[i].addEventListener('change', function() {
 
-            console.log(this + 'changed ' + this.checked);
             var posts = document.getElementsByClassName(this.id);
 
             for(var j = 0; j < posts.length; j++) {
                 if (this.checked) {
                     posts[j].style.display = 'block';
                 } else {
-                    posts[j].style.visibility = 'none';
+                    posts[j].style.display = 'none';
                 }
             }
         })
