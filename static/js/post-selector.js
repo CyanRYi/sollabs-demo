c56d2c43299cf73f95168ba6a1b0da0714cba19f
Cyan Raphael Yi <cyan.r.yi@gmail.com>
2019-03-13 07:48:11 +0900

    js 수정

--- static/js/post-selector.js
+++ static/js/post-selector.js
@@ -1,33 +1,34 @@
 (function () {
     var checkboxes = document.getElementsByClassName('mdl-checkbox__input');
 
-    for(var i = 0; i < checkboxes.length; i++) {
+    function togglePosts(id, checked) {
 
-        checkboxes[i].addEventListener('change', function() {
+        var posts = document.getElementsByClassName(id);
 
-            var posts = document.getElementsByClassName(this.id);
-
-            for(var j = 0; j < posts.length; j++) {
-                if (this.checked) {
-                    posts[j].style.display = 'block';
-                } else {
-                    posts[j].style.display = 'none';
-                }
-            }
-        })
-    }
-
-    function setDefault(pageType, pageName) {
-
-        if (pageType && pageName) {
-            var resetCheckboxes = document.querySelector('input[type=checkbox][id^=#tag_]');
-
-            for(var i = 0; i < resetCheckboxes.length; i++) {
-
-                if (resetCheckboxes[i].id !== 'tag_' + pageName) {
-                    resetCheckboxes[i].checked = false;
-                }
+        for(var j = 0; j < posts.length; j++) {
+            if (checked) {
+                posts[j].style.display = 'block';
+            } else {
+                posts[j].style.display = 'none';
             }
         }
     }
+
+    for(var i = 0; i < checkboxes.length; i++) {
+
+        checkboxes[i].addEventListener('change', function() {
+            togglePosts(this.id, this.checked);
+        })
+    }
+
+    function setDefault() {
+
+        var unchecked = document.querySelectorAll('input[type=checkbox]:not(:checked)');
+
+        for(var i = 0; i < unchecked.length; i++) {
+            togglePosts(unchecked[i].id, false);
+        }
+    }
+
+    setDefault();
 })();
