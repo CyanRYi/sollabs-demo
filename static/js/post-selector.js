517073ef0a6dfc42e3147c84d68fd266cf842ffb
Cyan Raphael Yi <cyan.r.yi@gmail.com>
2019-03-13 06:46:30 +0900

    vanillajs로 수정

--- static/js/post-selector.js
+++ static/js/post-selector.js
@@ -1,9 +1,17 @@
 (function () {
-    $(".mdl-checkbox__input").change(function () {
-        if (this.checked) {
-            $("." + this.id).css('visibility', 'visible');
-        } else {
-            $("." + this.id).css('visibility', 'hidden');
-        }
-    });
+    var checkboxes = document.getElementsByClassName('mdl-checkbox__input');
+
+    for(var i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].addEventListener('change', function() {
+            var posts = document.getElementsByClassName(checkboxes.id);
+
+            for(var j = 0; j < posts.length; j++) {
+                if (checkboxes[i].checked) {
+                    posts[j].style.visibility = 'visible';
+                } else {
+                    posts[j].style.visibility = 'hidden';
+                }
+            }
+        })
+    }
 })();
