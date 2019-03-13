c6eff7b04ad908f9c1bf306e2dd05dbc2c1dfa73
Cyan Raphael Yi <cyan.r.yi@gmail.com>
2019-03-14 02:42:46 +0900

    javascript 개선

--- static/js/super-search.js
+++ static/js/super-search.js
@@ -3,79 +3,102 @@
     MIT Licensed
 */
 
-(function () {
-    var isSearchOpen = false,
-        searchEl = document.querySelector('#js-search'),
-        searchInputEl = document.querySelector('#search__input'),
-        searchResultsEl = document.querySelector('#search__results'),
-        searchResultsOutline = document.querySelector('#search__outline'),
-        currentInputValue = '',
-        lastSearchResultHash,
-        posts = [];
+// Changes XML to JSON
+// Modified version from here: http://davidwalsh.name/convert-xml-json
+function xmlToJson(xml) {
 
-    // Changes XML to JSON
-    // Modified version from here: http://davidwalsh.name/convert-xml-json
-    function xmlToJson(xml) {
+    // Create the return object
+    var obj = {};
 
-        // Create the return object
-        var obj = {};
-
-        if (xml.nodeType == 1) { // element
-            // do attributes
-            if (xml.attributes.length > 0) {
+    if (xml.nodeType == 1) { // element
+        // do attributes
+        if (xml.attributes.length > 0) {
             obj["@attributes"] = {};
-                for (var j = 0; j < xml.attributes.length; j++) {
-                    var attribute = xml.attributes.item(j);
-                    obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
-                }
-            }
-        } else if (xml.nodeType == 3) { // text
-            obj = xml.nodeValue;
-        }
-
-        // do children
-        // If all text nodes inside, get concatenated text from them.
-        var textNodes = [].slice.call(xml.childNodes).filter(function (node) { return node.nodeType === 3; });
-        if (xml.hasChildNodes() && xml.childNodes.length === textNodes.length) {
-            obj = [].slice.call(xml.childNodes).reduce(function (text, node) { return text + node.nodeValue; }, '');
-        }
-        else if (xml.hasChildNodes()) {
-            for(var i = 0; i < xml.childNodes.length; i++) {
-                var item = xml.childNodes.item(i);
-                var nodeName = item.nodeName;
-                if (typeof(obj[nodeName]) == "undefined") {
-                    obj[nodeName] = xmlToJson(item);
-                } else {
-                    if (typeof(obj[nodeName].push) == "undefined") {
-                        var old = obj[nodeName];
-                        obj[nodeName] = [];
-                        obj[nodeName].push(old);
-                    }
-                    obj[nodeName].push(xmlToJson(item));
-                }
+            for (var j = 0; j < xml.attributes.length; j++) {
+                var attribute = xml.attributes.item(j);
+                obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
             }
         }
-        return obj;
+    } else if (xml.nodeType == 3) { // text
+        obj = xml.nodeValue;
     }
 
-    function getPostsFromXml(xml) {
-        var json = xmlToJson(xml);
-        return json.channel.item;
+    // do children
+    // If all text nodes inside, get concatenated text from them.
+    var textNodes = [].slice.call(xml.childNodes).filter(function (node) { return node.nodeType === 3; });
+    if (xml.hasChildNodes() && xml.childNodes.length === textNodes.length) {
+        obj = [].slice.call(xml.childNodes).reduce(function (text, node) { return text + node.nodeValue; }, '');
     }
+    else if (xml.hasChildNodes()) {
+        for(var i = 0; i < xml.childNodes.length; i++) {
+            var item = xml.childNodes.item(i);
+            var nodeName = item.nodeName;
+            if (typeof(obj[nodeName]) == "undefined") {
+                obj[nodeName] = xmlToJson(item);
+            } else {
+                if (typeof(obj[nodeName].push) == "undefined") {
+                    var old = obj[nodeName];
+                    obj[nodeName] = [];
+                    obj[nodeName].push(old);
+                }
+                obj[nodeName].push(xmlToJson(item));
+            }
+        }
+    }
+    return obj;
+}
 
-    var xmlhttp=new XMLHttpRequest();
-    xmlhttp.open("GET","/sitemap.xml");
-    xmlhttp.onreadystatechange = function () {
-        if (xmlhttp.readyState != 4) return;
-        if (xmlhttp.status != 200 && xmlhttp.status != 304) { return; }
-        var node = (new DOMParser).parseFromString(xmlhttp.responseText, 'text/xml');
-        node = node.children[0];
-        posts = getPostsFromXml(node);
+var posts = {
+
+    // get all post list from sitemap
+    init: function() {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("GET","/sitemap.xml");
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState != 4) return;
+            if (xmlhttp.status != 200 && xmlhttp.status != 304) { return; }
+            var node = (new DOMParser).parseFromString(xmlhttp.responseText, 'text/xml');
+            node = node.children[0];
+            this.contents = xmlToJson(node).channel.item;
+
+            console.log(posts);
+            if (contents.title !== undefined) {
+                this.contents = [this.contents];
+            }
+        }
+        xmlhttp.send();
+    },
+
+    getMatchedPosts: function(param) {
+        var result = contents.filter(function (post) {
+            if (post.title.toLowerCase().indexOf(param) !== -1
+                || post.description.toLowerCase().indexOf(param) !== -1) {
+                return true;
+            }
+        });
+
+        if (!result) {
+            return;
+        } else if (result.title !== undefined) {
+            return [result];
+        } else {
+            return result;
+        }
     }
-    xmlhttp.send();
+};
+
+(function () {
+
+    var postContainer = Object.create(posts);
+
+    var searchEl = document.getElementById('js-search'),
+        searchInputEl = document.getElementById('search__input'),
+        searchResultsEl = document.getElementById('search__results'),
+        searchResultsOutline = document.getElementById('search__outline'),
+        searchClearEl = document.getElementById('search__clear');
 
     window.toggleSearch = function toggleSearch() {
-        _gaq.push(['_trackEvent', 'supersearch', searchEl.classList.contains('is-active')]);
+        //_gaq.push(['_trackEvent', 'supersearch', searchEl.classList.contains('is-active')]);
         searchEl.classList.toggle('is-active');
         if (searchEl.classList.contains('is-active')) {
             // while opening
@@ -100,43 +123,40 @@
         }
     });
 
-    searchInputEl.addEventListener('input', function onInputChange() {
-        var currentResultHash, d;
+    searchClearEl.addEventListener('click', resetComponent);
 
-        currentInputValue = (searchInputEl.value + '').toLowerCase();
-        if (!currentInputValue || currentInputValue.length < 3) {
-            lastSearchResultHash = '';
-            searchResultsEl.classList.add('is-hidden');
+    var lastSearchResultHash = '';
+    searchInputEl.addEventListener('input', function onInputChange() {
+        var currentInputValue = (searchInputEl.value + '').toLowerCase();
+        if (currentInputValue.length < 3) {
+            resetComponent();
             return;
         }
-        searchResultsEl.style.offsetWidth;
 
-        var matchingPosts;
-        // check the `posts` object is single or many objects.
-        // if posts.title === undefined, so posts is many objects.
-        if(posts.title === undefined) {
-          matchingPosts = posts.filter(function (post) {
-              if ((post.title + '').toLowerCase().indexOf(currentInputValue) !== -1 || (post.description + '').toLowerCase().indexOf(currentInputValue) !== -1) {
-                  return true;
-              }
-          });
-        }else {
-          matchingPosts = [posts]; // assign single object to Array
+        var matchingPosts = postContainer.getMatchedPosts(currentInputValue);
+
+        console.log(matchingPosts);
+        if (matchingPosts.length === 0) {
+            resetComponent();
+            return;
         }
-        if (!matchingPosts.length) {
-            searchResultsEl.classList.add('is-hidden');
-            searchResultsOutline.classList.add('is-hidden');
-        }
-        currentResultHash = matchingPosts.reduce(function(hash, post) { return post.title + hash; }, '');
-        if (matchingPosts.length && currentResultHash !== lastSearchResultHash) {
+
+        var currentResultHash = matchingPosts.reduce(function(hash, post) { return post.title + hash; }, '');
+        if (currentResultHash !== lastSearchResultHash) {
             searchResultsEl.classList.remove('is-hidden');
             searchResultsOutline.classList.remove('is-hidden');
             searchResultsEl.innerHTML = matchingPosts.map(function (post) {
-                d = new Date(post.pubDate);
+                var d = new Date(post.pubDate);
                 return '<li><a href="' + post.link + '">' + post.title + '<span class="search__result-date">' + d.toUTCString().replace(/.*(\d{2})\s+(\w{3})\s+(\d{4}).*/,'$2 $1, $3') + '</span></a></li>';
             }).join('');
         }
         lastSearchResultHash = currentResultHash;
     });
+
+    function resetComponent() {
+        lastSearchResultHash = '';
+        searchResultsEl.classList.add('is-hidden');
+        searchResultsOutline.classList.add('is-hidden');
+    }
 
 })();
